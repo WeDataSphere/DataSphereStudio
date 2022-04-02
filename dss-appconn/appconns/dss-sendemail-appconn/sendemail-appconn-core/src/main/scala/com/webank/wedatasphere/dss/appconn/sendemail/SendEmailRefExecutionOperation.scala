519d375948069eb9d31382b88bf81e1fa75be30f
ElishaZhang <1049909975@qq.com>
2022-04-02 09:46:32 +0800

    [bug fix]暂时注释部分代码

--- dss-appconn/appconns/dss-sendemail-appconn/sendemail-appconn-core/src/main/scala/com/webank/wedatasphere/dss/appconn/sendemail/SendEmailRefExecutionOperation.scala
+++ dss-appconn/appconns/dss-sendemail-appconn/sendemail-appconn-core/src/main/scala/com/webank/wedatasphere/dss/appconn/sendemail/SendEmailRefExecutionOperation.scala
@@ -20,7 +20,7 @@ import java.util.Properties
 
 import com.webank.wedatasphere.dss.appconn.sendemail.conf.SendEmailAppConnInstanceConfiguration
 import com.webank.wedatasphere.dss.appconn.sendemail.email.EmailSender
-import com.webank.wedatasphere.dss.appconn.sendemail.email.sender.SpringJavaEmailSender
+//import com.webank.wedatasphere.dss.appconn.sendemail.email.sender.SpringJavaEmailSender
 import com.webank.wedatasphere.dss.common.utils.ClassUtils
 import com.webank.wedatasphere.dss.standard.app.development.listener.common.CompletedExecutionResponseRef
 import com.webank.wedatasphere.dss.standard.app.development.operation.RefExecutionOperation
@@ -34,12 +34,12 @@ import org.springframework.mail.javamail.JavaMailSender
 import scala.collection.JavaConversions._
 
 class SendEmailRefExecutionOperation extends RefExecutionOperation with Logging {
-  val EMAIL_FROM_DEFAULT = "email.from.default"
-  val EMAIL_HOST = "email.host"
-  val EMAIL_USERNAME = "email.username"
-  val EMAIL_PASSWORD = "email.password"
-  val EMAIL_PORT = "email.port"
-  val EMAIL_PROTOCOL = "email.protocol"
+  //  val EMAIL_FROM_DEFAULT = "email.from.default"
+  //  val EMAIL_HOST = "email.host"
+  //  val EMAIL_USERNAME = "email.username"
+  //  val EMAIL_PASSWORD = "email.password"
+  //  val EMAIL_PORT = "email.port"
+  //  val EMAIL_PROTOCOL = "email.protocol"
 
   private var service:DevelopmentService = _
 
@@ -50,20 +50,42 @@ class SendEmailRefExecutionOperation extends RefExecutionOperation with Logging 
   private val emailSender = SendEmailAppConnInstanceConfiguration.getEmailSender
 
   override def execute(requestRef: ExecutionRequestRef): ResponseRef = {
-    val instanceConfig = this.service.getAppInstance.getConfig
-    val properties = new Properties()
-    instanceConfig.foreach {
-      case (key: String, value: Object) =>
-        properties.put(key, value.toString)
-    }
-    val springJavaEmailSender =  new SpringJavaEmailSender()
-    val javaMailSender = springJavaEmailSender.getJavaMailSender
-    javaMailSender.setHost(properties.getProperty(EMAIL_HOST))
-    javaMailSender.setPort(Integer.parseInt(properties.getProperty(EMAIL_PORT)))
-    javaMailSender.setUsername(properties.getProperty(EMAIL_USERNAME))
-    javaMailSender.setPassword(properties.getProperty(EMAIL_PASSWORD))
-    javaMailSender.setProtocol(properties.getProperty(EMAIL_PROTOCOL))
-    val emailSender = ClassUtils.getInstanceOrDefault(classOf[EmailSender],springJavaEmailSender)
+    //    val instanceConfig = this.service.getAppInstance.getConfig
+    //    val properties = new Properties()
+    //    instanceConfig.foreach {
+    //      case (key: String, value: Object) =>
+    //        properties.put(key, value.toString)
+    //    }
+    //    val springJavaEmailSender =  new SpringJavaEmailSender()
+    //    val javaMailSender = springJavaEmailSender.getJavaMailSender
+    //    javaMailSender.setHost(properties.getProperty(EMAIL_HOST))
+    //    javaMailSender.setPort(Integer.parseInt(properties.getProperty(EMAIL_PORT)))
+    //    javaMailSender.setUsername(properties.getProperty(EMAIL_USERNAME))
+    //    javaMailSender.setPassword(properties.getProperty(EMAIL_PASSWORD))
+    //    javaMailSender.setProtocol(properties.getProperty(EMAIL_PROTOCOL))
+    //    val emailSender = ClassUtils.getInstanceOrDefault(classOf[EmailSender],springJavaEmailSender)
+    //    val response = new CompletedExecutionResponseRef(200)
+    //    val email = Utils.tryCatch {
+    //      sendEmailAppConnHooks.foreach(_.preGenerate(requestRef))
+    //      val email = emailGenerator.generateEmail(requestRef)
+    //      emailContentParsers.foreach{
+    //        p => Utils.tryQuietly(p.parse(email))
+    //      }
+    //      emailContentGenerators.foreach{
+    //        g => Utils.tryQuietly(g.generate(email))
+    //      }
+    //      sendEmailAppConnHooks.foreach(_.preSend(requestRef, email))
+    //      email
+    //    }{ t =>
+    //      putErrorMsg("解析邮件内容失败！", t, response)
+    //      return response
+    //    }
+    //    Utils.tryCatch {
+    //     emailSender.send(email)
+    //      response.setIsSucceed(true)
+    //    }(putErrorMsg("发送邮件失败！", _, response))
+    //    response
+    logger.info(" sendemail initialize start ... ")
     val response = new CompletedExecutionResponseRef(200)
     val email = Utils.tryCatch {
       sendEmailAppConnHooks.foreach(_.preGenerate(requestRef))
@@ -77,11 +99,12 @@ class SendEmailRefExecutionOperation extends RefExecutionOperation with Logging 
       sendEmailAppConnHooks.foreach(_.preSend(requestRef, email))
       email
     }{ t =>
-      putErrorMsg("解析邮件内容失败！", t, response)
+      putErrorMsg("解析邮件内容失败!", t, response)
       return response
     }
+    logger.info(" sendemail initialize end ... ")
     Utils.tryCatch {
-     emailSender.send(email)
+      emailSender.send(email)
       response.setIsSucceed(true)
     }(putErrorMsg("发送邮件失败！", _, response))
     response
