0440b68d2abf87f0c728ad4235ecfe22e14ba9cd
xlinliu <arionliu@webank.com>
2024-09-26 17:05:01 +0800

    增加模板覆盖参数map.limit和reduce.limit

--- dss-orchestrator/orchestrators/dss-workflow/dss-linkis-node-execution/src/main/java/com/webank/wedatasphere/dss/linkis/node/execution/service/impl/BuildJobActionImpl.java
+++ dss-orchestrator/orchestrators/dss-workflow/dss-linkis-node-execution/src/main/java/com/webank/wedatasphere/dss/linkis/node/execution/service/impl/BuildJobActionImpl.java
@@ -223,6 +223,8 @@ public class BuildJobActionImpl implements BuildJobAction {
             startupMap.remove("spark.executor.instances");
             startupMap.remove("wds.linkis.engineconn.java.driver.memory");
             startupMap.remove("spark.conf");
+            startupMap.remove("mapreduce.job.running.map.limit");
+            startupMap.remove("mapreduce.job.running.reduce.limit");
             logger.info("after remove startup map:{}",startupMap.keySet());
         }
         Map<String, Object> configurationMap = TaskUtils.getMap(paramMapCopy, TaskConstant.PARAMS_CONFIGURATION);
