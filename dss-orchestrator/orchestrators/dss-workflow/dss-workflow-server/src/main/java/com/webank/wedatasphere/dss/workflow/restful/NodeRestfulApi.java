11b3729a5a65e18dbd63eab680c4e8a877eefb74
SunpengWan <825559776@qq.com>
2026-03-06 14:39:39 +0800

    Revert "回退工作流对Spark版本属性相关代码 #AI commit#"
    This reverts commit 4bf6bca34a5f63e3bf191ce7a0b2ec39ec6f2bef.

--- dss-orchestrator/orchestrators/dss-workflow/dss-workflow-server/src/main/java/com/webank/wedatasphere/dss/workflow/restful/NodeRestfulApi.java
+++ dss-orchestrator/orchestrators/dss-workflow/dss-workflow-server/src/main/java/com/webank/wedatasphere/dss/workflow/restful/NodeRestfulApi.java
@@ -196,6 +196,11 @@ public class NodeRestfulApi {
                 continue;
             }
 
+            // 不在白名单, 则取消sparkVersion选项
+            if(!isWhite && "sparkVersion".equalsIgnoreCase(nodeUi.getKey())){
+                continue;
+            }
+
             NodeUiVO nodeUiVO = new NodeUiVO();
             BeanUtils.copyProperties(nodeUi, nodeUiVO);
             nodeUiVO.setDesc(descriptionSupplier.apply(nodeUi));
