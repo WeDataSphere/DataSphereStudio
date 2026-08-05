1f0ed66ad11f644685edead2a9f34c16d8281eeb
SunpengWan <825559776@qq.com>
2026-08-05 14:39:47 +0800

    add log #AI commit#

--- dss-appconn/appconns/dss-datago-feishu-appconn/src/main/scala/com/webank/wedatasphere/dss/appconn/datagofeishu/DataGoFeishuRefExecutionOperation.scala
+++ dss-appconn/appconns/dss-datago-feishu-appconn/src/main/scala/com/webank/wedatasphere/dss/appconn/datagofeishu/DataGoFeishuRefExecutionOperation.scala
@@ -196,8 +196,7 @@ class DataGoFeishuRefExecutionOperation
           action.nextPollAt = System.currentTimeMillis() + action.executeRetryInterval
           logger.info("DataGo Feishu export in progress, dmId={}, taskId={}, poll={}",
             action.nodeParams.getDmId, action.taskId, Int.box(action.exportPollCount))
-          appendLog(action, "外发进行中（exporting），第 " + action.exportPollCount +"次，下次轮询: "
-            + action.executeRetryInterval + "ms 后")
+          appendLog(action,s"外发进行中（exporting）,第${action.exportPollCount} 次 下次轮询: ${action.executeRetryInterval} ms 后")
         case status =>
           // exported/exporting 之外的 status 视为异常
           throw new DataGoFeishuException(82007,
